dc603d69c93571571c2a49363be9b9b5d4968184
Stijn <debackere@strw.leidenuniv.nl>
2021-12-10 11:11:32 +0100

    Add loading/reading functions for Gaussian/Poisson likelihoods.

--- amulator/models/loading.py
+++ amulator/models/loading.py
@@ -2,7 +2,7 @@ from gpytorch.mlls import VariationalELBO, ExactMarginalLogLikelihood
 from ignite.handlers import Checkpoint
 import torch
 
-from amulator.likelihoods import SuperPoissonLikelihood, GaussianLikelihood
+from amulator.likelihoods import PoissonLikelihood, SuperPoissonLikelihood, GaussianLikelihood
 from amulator.models import GPModel, ExactGPModel
 from amulator.training.trainer import GPModelTrainer
 
@@ -21,9 +21,7 @@ def get_gaussian_model_trainer(
     optimizer_kwargs = {} if optimizer_kwargs is None else optimizer_kwargs
 
     X = dataloader.dataset[:]["X"]
-    y = dataloader.dataset[:]["y"]
-    super_poisson_ratio = dataloader.dataset[:]["super_poisson_ratio"]
-
+    yvar = dataloader.dataset[:]["yvar"]
     if learn_inducing_locations:
         # initialize GP model
         inducing_points = (
@@ -41,7 +39,7 @@ def get_gaussian_model_trainer(
     )
     likelihood = GaussianLikelihood(**likelihood_kwargs)
     mll = VariationalELBO(likelihood, model, num_data=len(dataloader.dataset))
-    criterion_kwargs = {"super_poisson_ratio": "super_poisson_ratio"}
+    criterion_kwargs = {"noise": "yvar"}
     if likelihood.mean:
         criterion_kwargs["model_mean"] = "model_mean"
 
@@ -55,6 +53,124 @@ def get_gaussian_model_trainer(
         criterion_kwargs=criterion_kwargs,
     )
     return model_trainer
+
+
+def read_gaussian_gp_model_trainer(
+        checkpoint_file,
+        dataloader,
+        optimizer,
+        log=True,
+        mean=True,
+):
+    """Load the model_trainer from the checkpoint file with state_dict for
+    the model, likelihood, mll and optimizer."""
+    checkpoint_info = torch.load(checkpoint_file)
+
+    inducing_points = checkpoint_info["model"]["variational_strategy.inducing_points"]
+    model = GPModel(inducing_points)
+    model.load_state_dict(checkpoint_info["model"])
+    likelihood = GaussianLikelihood(log=log, mean=mean)
+    likelihood.load_state_dict(checkpoint_info["likelihood"])
+    mll = VariationalELBO(likelihood, model, num_data=len(dataloader.dataset))
+    optimizer = optimizer(model.parameters())
+    optimizer.load_state_dict(checkpoint_info["optimizer"])
+
+    criterion_kwargs = {"noise": "yvar"}
+    if mean:
+        criterion_kwargs["model_mean"] = "model_mean"
+
+    return GPModelTrainer(
+        model=model,
+        likelihood=likelihood,
+        mll=mll,
+        optimizer=optimizer,
+        criterion_kwargs=criterion_kwargs,
+    )
+
+
+def get_poisson_model_trainer(
+        dataloader,
+        num_inducing,
+        lr,
+        learn_inducing_locations=True,
+        model_kwargs=None,
+        likelihood_kwargs=None,
+        optimizer_kwargs=None,
+):
+    model_kwargs = {} if model_kwargs is None else model_kwargs
+    likelihood_kwargs = {} if likelihood_kwargs is None else likelihood_kwargs
+    optimizer_kwargs = {} if optimizer_kwargs is None else optimizer_kwargs
+
+    X = dataloader.dataset[:]["X"]
+    if learn_inducing_locations:
+        # initialize GP model
+        inducing_points = (
+            torch.rand((num_inducing, X.shape[-1]))
+            * (X.max(dim=0).values - X.min(dim=0).values)
+        ) + X.min(dim=0).values
+    else:
+        inducing_points = (
+            (X - X.min(dim=0).values) / (X.max(dim=0).values - X.min(dim=0).values)
+        )
+
+    model = GPModel(
+        inducing_points,
+        learn_inducing_locations=learn_inducing_locations,
+        **model_kwargs,
+    )
+    likelihood = PoissonLikelihood(**likelihood_kwargs)
+    mll = VariationalELBO(likelihood, model, num_data=len(dataloader.dataset))
+    criterion_kwargs = {}
+    if likelihood.mean:
+        criterion_kwargs["model_mean"] = "model_mean"
+
+    # determine optimizer
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=lr,
+        **optimizer_kwargs,
+    )
+    model_trainer = GPModelTrainer(
+        model=model,
+        likelihood=likelihood,
+        mll=mll,
+        optimizer=optimizer,
+        criterion_kwargs=criterion_kwargs,
+    )
+    return model_trainer
+
+
+def read_poisson_gp_model_trainer(
+        checkpoint_file,
+        dataloader,
+        optimizer,
+        log=True,
+        mean=True,
+):
+    """Load the model_trainer from the checkpoint file with state_dict for
+    the model, likelihood, mll and optimizer."""
+    checkpoint_info = torch.load(checkpoint_file)
+
+    inducing_points = checkpoint_info["model"]["variational_strategy.inducing_points"]
+    model = GPModel(inducing_points)
+    model.load_state_dict(checkpoint_info["model"])
+    likelihood = PoissonLikelihood(log=log, mean=mean)
+    likelihood.load_state_dict(checkpoint_info["likelihood"])
+    mll = VariationalELBO(likelihood, model, num_data=len(dataloader.dataset))
+    optimizer = optimizer(model.parameters())
+    optimizer.load_state_dict(checkpoint_info["optimizer"])
+
+    criterion_kwargs = {}
+    if mean:
+        criterion_kwargs["model_mean"] = "model_mean"
+
+    return GPModelTrainer(
+        model=model,
+        likelihood=likelihood,
+        mll=mll,
+        optimizer=optimizer,
+        criterion_kwargs=criterion_kwargs,
+    )
 
 
 def get_super_poisson_model_trainer(
