60b4d1ab6d01855c4fac10d4eac4fa3dc4d193aa
Stijn <debackere@strw.leidenuniv.nl>
2022-01-19 14:53:59 +0100

    Fix super_poisson_likelihood so we fit dndlog10m instead of N.

--- amulator/likelihoods/super_poisson_likelihood.py
+++ amulator/likelihoods/super_poisson_likelihood.py
@@ -11,45 +11,54 @@ class SuperPoissonLikelihood(_OneDimensionalLikelihood):
     with variance :math:`\alpha N`, where :math:`\alpha > 1` is the
     poisson_ratio.
 
-    :param bool log: model predicts log counts :math:`\log N`
-    :param bool ratio: model predicts ratio to mean :math:`N / \maths N \rangle`
+    :param bool n2N: model predicts number density :math:`n = N / n2N` instead of :math:`N`
+    :param bool log: model predicts log :math:`\log N`
+    :param bool ratio: model predicts ratio to mean :math:`N / \langle N \rangle`
     """
-    def __init__(self, log=True, mean=True, likelihood_kwargs=None, *args, **kwargs):
+    def __init__(self, n2N=False, log=True, mean=True, likelihood_kwargs=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.n2N = n2N
         self.log = log
         self.mean = mean
         self.likelihood_kwargs = {} if likelihood_kwargs is None else likelihood_kwargs
 
     def _get_kwargs(self, model_samples, **kwargs):
+        """convert model_samples to correct counts"""
         if "poisson_ratio" not in kwargs:
             raise ValueError("'poisson_ratio' should be in kwargs")
 
         poisson_ratio = kwargs["poisson_ratio"]
         # ensure noise is always super-poisson
-        if torch.isnan(poisson_ratio).any():
-            warnings.warn("forcing nans in poisson_ratio to 1")
-            poisson_ratio[poisson_ratio.isnan()] = 1.
-
-        if (poisson_ratio < 1).any():
-            warnings.warn("forcing poisson_ratio >= 1")
-            poisson_ratio[poisson_ratio < 1.] = 1.
+        if torch.isnan(poisson_ratio).any() or (poisson_ratio < 1.).any():
+            raise ValueError("poisson_ratio contains NaNs or values < 1.")
+            # warnings.warn("forcing nans in poisson_ratio to 1")
+            # poisson_ratio[poisson_ratio.isnan()] = 1.
 
         if self.log:
             model_samples = model_samples.exp()
 
+        # model predicts ratio to mean
         if self.mean:
             if "model_mean" not in kwargs:
                 raise ValueError("model_mean needs to be specified if {self.mean=}")
 
-            else:
-                model_mean = kwargs["model_mean"]
-                if self.log:
-                    model_mean = model_mean.exp()
+            model_mean = kwargs["model_mean"]
+            if torch.any(model_mean == 0.):
+                warnings.warn("model_mean contains 0.")
 
-                if torch.any(model_mean == 0.):
-                    warnings.warn("model_mean contains 0.")
+            if log:
+                model_mean = model_mean.exp()
 
+            # take out ratio
             model_samples = model_samples * model_mean
+
+        # convert to actual counts if normalized
+        if self.n2N:
+            if "n2N" not in kwargs:
+                raise ValueError("'n2N' needs to be in kwargs specified if {self.n2N=}")
+
+            n2N = kwargs["n2N"]
+            model_samples = model_samples * n2N
 
         # ensure poisson_ratio > 1 to avoid divergences in r & probs
         alpha = poisson_ratio + 1e-6
@@ -63,6 +72,8 @@ class SuperPoissonLikelihood(_OneDimensionalLikelihood):
         r = model_samples / (alpha - 1)
         probs = 1 - alpha ** (-1)
 
+        # ensure small value to prevent NaN's in log_prob
+        r[r == 0.] = 1e-6
         return {
             "total_count": r,
             "probs": probs,
