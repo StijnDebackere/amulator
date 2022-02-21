717867143cba715692b38a1e57947bb8d94e2ead
Stijn <debackere@strw.leidenuniv.nl>
2022-02-21 11:19:06 +0100

    Revert "Remove std from SuperPoissonLikelihoodMean."
    This reverts commit 7319f09328a920719c84a8b78424f6db05bdfb13.

--- amulator/likelihoods/__init__.py
+++ amulator/likelihoods/__init__.py
@@ -1,3 +1,3 @@
 from .gaussian_likelihood import GaussianLikelihoodMeanStd
 from .poisson_likelihood import PoissonLikelihoodMeanStd
-from .super_poisson_likelihood import SuperPoissonLikelihoodMean
+from .super_poisson_likelihood import SuperPoissonLikelihoodMeanStd
